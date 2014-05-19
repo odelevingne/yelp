12a4e4008be150e56d1a2492bbc2c13a20ade44c
Oliver Delevingne <oliver@delevingne.co.uk>
2014-05-19 17:15:22 +0100

    implemented 'create' action for restaurant controller. can now create a new restaurant in the database. tests passing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,16 @@
 class RestaurantsController < ApplicationController
 
 	def index
+		@restaurants = Restaurant.all
 	end
 
 	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		Restaurant.create({name: 'McDonalds', address: '1 City Road, London'})
+		redirect_to '/restaurants'
 	end
 
 end
