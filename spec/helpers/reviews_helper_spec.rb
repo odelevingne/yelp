710e806296860b969d747db023f742220f266c31
Oliver Delevingne <oliver@delevingne.co.uk>
2014-05-22 16:28:50 +0100

    star ratings. started writing tests for reviews helper method 'star_rating'

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ReviewsHelper. For example:
-#
-# describe ReviewsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe ReviewsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe '#star_rating' do
+		it 'returns 5 filled stars for a rating of 5' do
+			expect(star_rating(5)).to eq '★★★★★'
+		end
+
+		it 'returns x filled stars, with the remainder white stars, if x < 5' do
+		end
+
+		specify 'if value is not a number, return it unchanged' do
+		end
+
+		it 'rounds to the nearest whole star' do
+		end
+	end
 end
+
+
+
+# ★★★☆☆
