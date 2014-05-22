416b3a11b8fd6a79a7472a66f09e1105ec665864
Oliver Delevingne <oliver@delevingne.co.uk>
2014-05-22 14:10:33 +0100

    added average rating to the index html. feature test for showing average rating now passes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,11 +3,14 @@ class Restaurant < ActiveRecord::Base
 	validates :name, format: { with: /\A[A-Z]/, message: 'must begin with capital letter' }
 	has_many :reviews
 
+
 	def average_rating
 		if reviews.any?
-			reviews.inject(0) {|total, review|
-				total + review.rating
-				} / reviews.count.to_f
+			 reviews.average(:rating)
+			# reviews.inject(0) {|total, review|
+			# 	total + review.rating
+			# 	} / reviews.count.to_f 
+			# the three lines above are equivalent to line 9. Line 9 is a rails method. Handy.
 		else
 			'N/A'
 		end
